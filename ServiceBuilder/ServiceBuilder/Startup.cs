314c4a4ea3c8f53cb9aade10ca1f596acd7960d0
CasperAJ <casper.rts.skole@gmail.com>
2017-12-04 15:17:34 +0100

    Creation

---
+++ ServiceBuilder/ServiceBuilder/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceBuilder.Startup))]
+namespace ServiceBuilder
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
